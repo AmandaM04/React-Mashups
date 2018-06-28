eb94c1aa427bec9f319a95a02f22cbe2b16c9cef
Amanda Mitchell <wreedamitchell@gmail.com>
2018-06-27 20:56:36 -0500

    listing names showing

--- src/components/Animals/Animals.js
+++ src/components/Animals/Animals.js
@@ -4,9 +4,16 @@ import './Animals.css';
 
 class Animals extends React.Component {
   render () {
+    const { animals } = this.props;
+    const animalItemComponents = animals.map((animal) => {
+      return (
+        <div key={animal.id}>{animal.name}</div>
+      );
+    });
     return (
       <div className="Animals">
         <h4>Animals</h4>
+        <div className="thumbnail">{animalItemComponents}</div>
       </div>
     );
   }
