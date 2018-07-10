e4dcbd7e75979e86c94ed8577ce44fc36b27b564
Amanda Mitchell <wreedamitchell@gmail.com>
2018-07-10 09:20:16 -0500

    animals showing and new form showing

--- src/components/Animals/Animals.js
+++ src/components/Animals/Animals.js
@@ -7,13 +7,21 @@ class Animals extends React.Component {
     const { animals } = this.props;
     const animalItemComponents = animals.map((animal) => {
       return (
-        <div key={animal.id}>{animal.name}</div>
+        <div key={animal.id}>
+          <div className="animalCard">
+            <div className="thumbnail col-sm-4">
+              <img src={animal.imgUrl} alt="animal pic" />
+              <h3>{animal.name}</h3>
+              <p>{animal.description}</p>
+            </div>
+          </div>
+        </div>
       );
     });
     return (
       <div className="Animals">
         <h4>Animals</h4>
-        <div className="thumbnail">{animalItemComponents}</div>
+        {animalItemComponents}
       </div>
     );
   }
--- src/components/ListingForm/ListingForm.js
+++ src/components/ListingForm/ListingForm.js
@@ -2,11 +2,87 @@ import React from 'react';
 
 import './ListingForm.css';
 
+const defaultListing = {
+  name: '',
+  imageUrl: '',
+  description: '',
+};
+
 class ListingForm extends React.Component {
+  state = {
+    newListing: defaultListing,
+  }
+
+  nameChange = (e) => {
+    this.formFieldStringState('name', e);
+  }
+
+  imageUrlChange = (e) => {
+    this.formFieldStringState('imageUrl', e);
+  }
+
+  descriptionChange = e => {
+    this.formFieldStringState('description', e);
+  };
+
+  formSubmit = (e) => {
+    const { onSubmit } = this.props;
+    const { newListing } = this.props;
+    e.preventDefault();
+    if (
+      newListing.name &&
+      newListing.imageUrl &&
+      newListing.description
+    ) {
+      onSubmit(this.state.newListing);
+      this.setState({ newListing: defaultListing });
+    } else {
+      alert('dear god why????');
+    }
+  }
   render () {
+    const { newListing } = this.state;
     return (
       <div className="ListingForm">
-        <h4>Add New Animal</h4>
+        <h4 className="text-center">Add New Animal:</h4>
+        <form>
+          <fieldset className="col-xs-12">
+            <label htmlFor="name">Name:</label>
+            <input
+              className="col-xs-12"
+              type="text"
+              id="name"
+              placeholder="Name"
+              value={newListing.name}
+              onChange={this.nameChange}
+            />
+          </fieldset>
+          <fieldset className="col-xs-12">
+            <label htmlFor="imageUrl">Image Url:</label>
+            <input
+              className="col-xs-12"
+              type="text"
+              id="imageUrl"
+              placeholder="http://www.google.com"
+              value={newListing.imageUrl}
+              onChange={this.imageUrlChange}
+            />
+          </fieldset>
+          <fieldset className="col-xs-12">
+            <label htmlFor="description">Description:</label>
+            <input
+              className="col-xs-12"
+              type="text"
+              id="description"
+              placeholder="description"
+              value={newListing.description}
+              onChange={this.descriptionChange}
+            />
+          </fieldset>
+          <button className="col-xs-4 btn btn-primary col-xs-offset-4">
+            Add mashup
+          </button>
+        </form>
       </div>
     );
   }
